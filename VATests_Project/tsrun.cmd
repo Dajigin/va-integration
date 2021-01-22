c21528bc986c47ec7c8b877686ba7abdecd09eba
Dmitry Azhigin <dajigin@mail.ru>
2021-01-22 16:02:27 +0300

    Allure report added.Refactoring.

--- VATests_Project/tsrun.cmd
+++ VATests_Project/tsrun.cmd
@@ -1,1 +1,1 @@
-oscript .\tsrun.os --v8-version 8.3.14.1976 --testspath "D:\_DEVELOP\1S\TestsIntegration\Repo\VATests_Project" --testsetname "Группа тестов\Справочник Контрагенты"  --ibconnection "D:\_DEVELOP\1S\TestsIntegration\SSLDemoBase" --dbuser "Администратор" --run
+oscript .\tsrun.os --v8-version 8.3.14.1976 --testspath "D:\_DEVELOP\1S\TestsIntegration\Repo\VATests_Project" --testsetname "Группа тестов\Справочник Контрагенты"  --ibconnection "D:\_DEVELOP\1S\TestsIntegration\SSLDemoBase" --dbuser "Администратор" --allure_show --run 
