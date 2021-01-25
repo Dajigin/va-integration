6c4c3d9d7e2703066b54c2fc997b70bd560a2dae
Dmitry Azhigin <dajigin@mail.ru>
2021-01-25 16:36:57 +0300

    Single tests set execution realised

---
+++ VATests_Project/Exec/tsrun_sample_set.cmd
@@ -0,0 +1,1 @@
+oscript .\tsrun.os --v8-version 8.3.14.1976 --testspath "D:\_DEVELOP\1S\TestsIntegration\Repo\VATests_Project" --testsetname "Группа тестов\Документ Оприходование товаров.tsc"  --ibconnection "D:\_DEVELOP\1S\TestsIntegration\SSLDemoBase" --dbuser "Администратор" --allure_show --run --debuglevel 0
